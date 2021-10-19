8bd9b761a48b341d37e63375515b87afb804dce4
Dan Nguyen <ndan0112@gmail.com>
2021-10-19 09:19:58 -0500

    Movement, Confirm, Reset and Timeout
    Movement functionality
    -Up Left Right Down
    - Out of bound check
    
    Confirm
    - Currently Confirm is expressed by the blinking of LED
    
    Reset
    -Will turn on the first item that is indicated in the array of LED lights
    
    Timeout
    - Take in the config TIMEOUT and check if the vending machine is not in used
    - If TIMEOUT, turn off all LED, and reset the position to the first item in LED array.

---
+++ VendMachineDemo/VendMachineDemo.ino
@@ -0,0 +1,148 @@
+//LED pin definitions
+const int Red1=30;
+const int Red2=29;
+const int Grn1=28;
+const int Grn2=27;
+const int Blu1=26;
+const int Blu2=25;
+const int Ylw1=24;
+const int Ylw2=23;
+
+//buttons pin definitions (should be changed when the button pin is known
+const int upButton = 0;
+const int leftButton = 1;
+const int rightButton = 2;
+const int downButton = 3;
+const int resetButton = 4;
+const int confirmButton = 5;
+
+//CONFIG
+int TIMEOUT = 10; //In Minutes
+
+int maxX = 2;
+int maxY = 4;
+int positionLed[4][2] = {{Red1,Red2},{Grn1,Grn2},{Blu1,Blu2},{Ylw1,Ylw2}};
+
+int maxButtons = 6;
+int buttons[6] = {upButton,leftButton,rightButton,downButton,resetButton,confirmButton};
+
+
+
+
+
+
+
+//Below is code that doesn't need to be changed unless my code is poo poo :(((
+int r = 0, c = 0;
+int timePassed = 0;
+
+bool checkTimeOut(){
+  if (timePassed > TIMEOUT * 1000){
+    return true;
+  }else{
+    return false;
+  }
+}
+void OrderConfirmed(){
+  //Check if it is in timeout
+  if (checkTimeOut()){
+    return;
+  }
+  //the order successfully confirmed
+  //Do something lol
+  for (int i = 0; i < 5; i++){
+   digitalWrite(positionLed[r][c], LOW);
+   delay(500);
+   digitalWrite(positionLed[r][c], HIGH);
+   delay(500);
+  }
+  
+  //Reset the machine
+  r = 0, c = 0;
+  
+}
+
+void buttonPressed(int input){
+ if (digitalRead(input)) {
+   switch(input) {
+     case upButton:{//Up
+       r--;
+       break;
+     }case leftButton:{//Left
+       c--;
+       break;
+     }case rightButton:{//Right
+       c++;
+       break;
+     }case downButton:{//Down
+       r++;
+       break;
+     }case resetButton:{//Reset
+       r = 0, c = 0;
+       break;
+     }case confirmButton:{//Confirm the order, should set motor power
+       OrderConfirmed();
+       return;
+     }
+  }
+   
+   //check for out of bound, and set it to location
+   if (r >= maxY){
+     r = 0;
+   }else if (r < 0){
+     r = maxY - 1;
+   }
+   if (c >= maxX){
+     c = 0;
+   }else if (c < 0){
+     c = maxX - 1;
+   }
+   
+   do {//Keep looping if the button is still pressed
+     if (timePassed >= TIMEOUT * 1000){
+      r = 0, c = 0;
+     }
+     timePassed = 0;
+   }while(digitalRead(input) == 1);
+  }
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  //8 output LEDs
+  for (int i = 0; i < maxY; i++){
+    for (int j = 0; j < maxX; j++){
+      pinMode(positionLed[i][j],OUTPUT);
+    }
+  }
+
+  //6 Input Buttons
+  for (int i = 0; i < maxButtons; i++){
+    pinMode(buttons[i],OUTPUT);
+  }
+  
+  
+}
+
+void loop() {
+  //turn everything off
+  for (int i = 0; i < maxY; i++){
+    for (int j = 0; j < maxX; j++){
+      digitalWrite(positionLed[i][j],LOW);
+    }
+  }
+  
+  //check if any buttons is pressed
+  for (int i = 0; i < maxButtons; i++){
+    buttonPressed(buttons[i]);
+  }
+  
+  //timeOut
+  timePassed++;
+  if (checkTimeOut()){
+    return;
+  }
+  digitalWrite(positionLed[r][c],HIGH);
+
+  delay(1); //Is this needed? This help with the timeout function that I set up
+}
